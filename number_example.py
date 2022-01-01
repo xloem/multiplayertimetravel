15bf22465b13b906abfc261dcd9a9d7f546164cd
xloem <0xloem@gmail.com>
2021-12-31 21:24:27 -0500

    -

--- number_example.py
+++ number_example.py
@@ -6,11 +6,12 @@
 #         another state based on a time change from the first
 #         number = constant * time
 
-# goal 2: model user in a way that can be simulated, and let them mutate
-#         the constant, spawning temporal uncertainty. the simulation can
-#         be simply a uniform distribution of chance of change.
+# goal 2: model user in a way that can be simulated, to mutate
+#         the constant. the simulation can be simply a uniform
+#         distribution of chance of change.
 
-# goal 3: simulate a single timeline, letting the user move along it arbitrarily
+# goal 3: simulate a single timeline, letting the user move along it arbitrarily,
+#         using the user's model of behavior to predict state.
 
 # goal 4: simulate multiple timelines, with an interface as to whether to bind
 #         actions to one or spawn a new one.  be nice to have that be a float
